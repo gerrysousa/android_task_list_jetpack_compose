f5f022819e1db51b2a7c2a0ad8d8d10dce6e69d2
Gerry Souza <gerry.souza-ext@ab-inbev.com>
2024-09-08 17:33:27 -0300

    Jetpack Compose - Criando o Layout Principal do App Lista de Tarefas - Android Studio Tutorial

--- ListadetarefaCompose/app/src/main/java/com/gdev/listadetarefacompose/view/ListaTarefas.kt
+++ ListadetarefaCompose/app/src/main/java/com/gdev/listadetarefacompose/view/ListaTarefas.kt
@@ -1,10 +1,60 @@
 package com.gdev.listadetarefacompose.view
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.gdev.listadetarefacompose.R
+import com.gdev.listadetarefacompose.ui.theme.BLACK
+import com.gdev.listadetarefacompose.ui.theme.Purple40
+import com.gdev.listadetarefacompose.ui.theme.WHITE
 
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun ListaTarefas(navController: NavController) {
-    Text(text = "Lista de Tarefas")
+fun ListaTarefas(
+    navController: NavController
+) {
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "Lista de Tarefas",
+                        color = WHITE,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                },
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = Purple40
+                )
+            )
+        },
+        containerColor = BLACK,
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {navController.navigate("salvarTarefa")},
+                containerColor = Purple40,
+            ){
+              Image(
+                  imageVector = ImageVector.vectorResource(id = R.drawable.ic_add),
+                  contentDescription ="Icone de adicionar tarega"
+              )
+            }
+        }
+    ) {
+    }
+
 }
--- ListadetarefaCompose/app/src/main/java/com/gdev/listadetarefacompose/view/SalvarTarefa.kt
+++ ListadetarefaCompose/app/src/main/java/com/gdev/listadetarefacompose/view/SalvarTarefa.kt
@@ -1,9 +1,18 @@
 package com.gdev.listadetarefacompose.view
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.gdev.listadetarefacompose.ui.theme.WHITE
 
 @Composable
 fun SalvarTarefa(navController: NavController) {
-
+    Text(
+        text = "Tela de Salvar Tarefa",
+        color = WHITE,
+        fontSize = 18.sp,
+        fontWeight = FontWeight.Bold
+    )
 }
