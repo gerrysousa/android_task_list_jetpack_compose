3b1e4b1a7905b345762cae73ea47a756e0eb7392
Gerry Souza <gerry.souza-ext@ab-inbev.com>
2024-09-09 21:05:59 -0300

    Jetpack Compose - Criando uma Lista de Tarefas - Android Studio Tutorial

---
+++ ListadetarefaCompose/app/src/main/java/com/gdev/listadetarefacompose/itemlist/TarefaItem.kt
@@ -0,0 +1,110 @@
+package com.gdev.listadetarefacompose.itemlist
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.gdev.listadetarefacompose.R
+import com.gdev.listadetarefacompose.model.Tarefa
+import com.gdev.listadetarefacompose.ui.theme.BLACK
+import com.gdev.listadetarefacompose.ui.theme.RADIO_BUTTON_GREEN_SELECTED
+import com.gdev.listadetarefacompose.ui.theme.RADIO_BUTTON_RED_SELECTED
+import com.gdev.listadetarefacompose.ui.theme.RADIO_BUTTON_YELLOW_SELECTED
+import com.gdev.listadetarefacompose.ui.theme.ShapeCardPrioridade
+import com.gdev.listadetarefacompose.ui.theme.WHITE
+
+@Composable
+fun TarefaItem(
+    position: Int,
+    listaTarefa: MutableList<Tarefa>
+) {
+    val titulo = listaTarefa[position].tarefa
+    val descricao = listaTarefa[position].descricao
+    val prioridade = listaTarefa[position].prioridade
+
+    var nivelPrioridade = when (prioridade) {
+        0 -> "Sem prioridade"
+        1 -> "Prioridade Baixa"
+        2 -> "Prioridade Media"
+        else -> "Prioridade Alta"
+    }
+
+    var colorPrioridade = when (prioridade) {
+        0 -> BLACK
+        1 -> RADIO_BUTTON_GREEN_SELECTED
+        2 -> RADIO_BUTTON_YELLOW_SELECTED
+        else -> RADIO_BUTTON_RED_SELECTED
+    }
+
+    Card(
+        colors = CardDefaults.cardColors(containerColor = WHITE),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+    ) {
+        ConstraintLayout(modifier = Modifier.padding(20.dp)) {
+            val (txtTarefa, txtDescricao, txtPrioridade, cardPrioridade, btnDeletar) = createRefs()
+
+            Text(
+                text = titulo.toString(),
+                modifier = Modifier.constrainAs(txtTarefa) {
+                    top.linkTo(parent.top, margin = 10.dp)
+                    start.linkTo(parent.start, margin = 10.dp)
+                }
+            )
+
+            Text(
+                text = descricao.toString(),
+                modifier = Modifier.constrainAs(txtDescricao) {
+                    top.linkTo(txtTarefa.bottom, margin = 10.dp)
+                    start.linkTo(parent.start, margin = 10.dp)
+                }
+            )
+
+            Text(
+                text = nivelPrioridade,
+                modifier = Modifier.constrainAs(txtPrioridade) {
+                    top.linkTo(txtDescricao.bottom, margin = 10.dp)
+                    start.linkTo(parent.start, margin = 10.dp)
+                }
+            )
+
+            Card(
+                colors = CardDefaults.cardColors(containerColor = colorPrioridade),
+                modifier = Modifier
+                    .size(30.dp)
+                    .constrainAs(cardPrioridade) {
+                        top.linkTo(txtDescricao.bottom, margin = 10.dp)
+                        start.linkTo(txtPrioridade.end, margin = 10.dp)
+                        bottom.linkTo(parent.bottom, margin = 10.dp)
+                    },
+                shape = ShapeCardPrioridade.large
+            ) { }
+
+            IconButton(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.constrainAs(btnDeletar) {
+                    top.linkTo(txtDescricao.bottom, margin = 10.dp)
+                    start.linkTo(cardPrioridade.end, margin = 30.dp)
+                    end.linkTo(parent.end, margin = 10.dp)
+                    bottom.linkTo(parent.bottom, margin = 10.dp)
+                }
+            ) {
+                Image(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_delete),
+                    contentDescription = "Icone de deletar"
+                )
+            }
+        }
+    }
+}
--- ListadetarefaCompose/app/src/main/java/com/gdev/listadetarefacompose/view/ListaTarefas.kt
+++ ListadetarefaCompose/app/src/main/java/com/gdev/listadetarefacompose/view/ListaTarefas.kt
@@ -2,6 +2,10 @@ package com.gdev.listadetarefacompose.view
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Scaffold
@@ -9,12 +13,15 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.gdev.listadetarefacompose.R
+import com.gdev.listadetarefacompose.itemlist.TarefaItem
+import com.gdev.listadetarefacompose.model.Tarefa
 import com.gdev.listadetarefacompose.ui.theme.BLACK
 import com.gdev.listadetarefacompose.ui.theme.Purple40
 import com.gdev.listadetarefacompose.ui.theme.WHITE
@@ -54,6 +61,33 @@ fun ListaTarefas(
             }
         }
     ) {
-    }
+        val listaTarefa: MutableList<Tarefa> = mutableListOf(
+            Tarefa(
+                tarefa = "Tarefa 0",
+                descricao = "Descrição da Tarefa 0",
+                prioridade = 0
+            ),
+            Tarefa(
+                tarefa = "Tarefa 1",
+                descricao = "Descrição da Tarefa 1",
+                prioridade = 1
+            ),
+            Tarefa(
+                tarefa = "Tarefa 2",
+                descricao = "Descrição da Tarefa 2",
+                prioridade = 2
+            ),
+            Tarefa(
+                tarefa = "Tarefa 3",
+                descricao = "Descrição da Tarefa 3",
+                prioridade = 3
+            ),
+        )
 
+        LazyColumn {
+            itemsIndexed(listaTarefa) { position, _ ->
+                TarefaItem(position, listaTarefa)
+            }
+        }
+    }
 }
